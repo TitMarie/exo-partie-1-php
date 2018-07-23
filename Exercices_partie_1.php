5e844cff8e6f7234c612ca4b90ef4d99b29dd3fa
Marie <marie-france.gauthier@et.simplon-roanne.com>
2018-07-23 13:31:05 +0200

    coucou

---
+++ Exercices_partie_1.php
@@ -0,0 +1,57 @@
+<?php
+// exercice 1
+
+$name = "Marie";
+
+// exercice 2
+
+$lastName = "Bouh";
+$firstName = "BB";
+$age = 5;
+
+// echo $name ." ". $lastName ." ". $firstName ." ". $age;
+
+// exercice 3
+
+$km = 1;
+
+echo $km ." ";
+
+$km = 3;
+
+echo $km ." ";
+
+$km = 125;
+
+echo $km."<br />";
+
+// exerciec 4
+
+$string = "pi";
+$int = 3;
+$float = 3.1415926;
+$boolean = true;
+echo $string ." ". $int ." ". $float ." ". $boolean ."<br>";
+
+// exercice 5
+
+$int2 = NULL;
+echo $int2;
+$int2 = 314;
+echo $int2 ."<br>";
+
+// exercice 6
+
+$name2 = "Bob l'éponge";
+echo "Bonjour ". $name2 .", comment vas-tu ?";
+
+// exercice 7
+
+echo "<br> Bonjour ". $lastName ." ". $firstName .", tu as ". $age ." ans.<br>";
+
+// exercice 8
+
+$addition = 3 + 5;
+$multiplication = 5 * 20;
+$soustraction = 45 / 5;
+echo $addition ." ". $multiplication ." ". $soustraction;
